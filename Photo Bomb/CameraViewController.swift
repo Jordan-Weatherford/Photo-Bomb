25e9c09a8a2f6f304fec5d6740a9cba0dda0ba38
Jordan-Weatherford <jrw821@gmail.com>
2017-05-30 16:49:08 -0700

    got camera working

--- Photo Bomb/CameraViewController.swift
+++ Photo Bomb/CameraViewController.swift
@@ -7,29 +7,97 @@
 //
 
 import UIKit
+import AVFoundation
+import AlamoFire
 
-class CameraViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, AVCapturePhotoCaptureDelegate {
     
 
-    /*
-    // MARK: - Navigation
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var captureButton: UIButton!
+    
+    var captureSession : AVCaptureSession?
+    var stillImageOutput : AVCapturePhotoOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    var image : UIImage?
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureButton.layer.zPosition = 100
+        previewLayer?.frame = cameraView.bounds
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession = AVCaptureSession()
+        
+        
+        captureSession?.sessionPreset = AVCaptureSessionPresetHigh
+        
+        let backCamera = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        var backCameraError : Error?
+        var input : AVCaptureDeviceInput?
+        
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch {
+            backCameraError = error
+        }
+        
+        if backCameraError == nil && (captureSession?.canAddInput(input))! {
+            captureSession?.addInput(input)
+            stillImageOutput = AVCapturePhotoOutput()
+            
+            if (captureSession?.canAddOutput(stillImageOutput))! {
+                captureSession?.addOutput(stillImageOutput)
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.portrait
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession?.startRunning()
+            }
+        }
+    }
+    
+    
+    @IBAction func captureButtonPressed(_ sender: UIButton) {
+        didPressTakePhoto()
+    }
+    
+    
+    func didPressTakePhoto() {
+        if let videoConnection = stillImageOutput?.connection(withMediaType: AVMediaTypeVideo) {
+            videoConnection.videoOrientation = AVCaptureVideoOrientation.portrait
+            let photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecJPEG])
+            stillImageOutput?.capturePhoto(with: photoSettings, delegate: self)
+        }
+    }
+    
+    
+    //AVPhotoCapture Delegate function
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?){
 
+        if let error = error {
+            print(error.localizedDescription)
+        }
+        
+        if let sampleBuffer = photoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: nil){
+            image = UIImage(data: dataImage)
+        } else {
+            print("FAILED AT IMAGE PROCESSING")
+        }
+    }
 }
+
